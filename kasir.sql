10ebb178cf42170473c0e66e4a2f5f2e7a8b212e
Muhamad Nabil <132574272+Aaronabil@users.noreply.github.com>
2025-02-27 23:23:57 +0700

    db yang bener

---
+++ kasir.sql
@@ -0,0 +1,234 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 27, 2025 at 02:36 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kasir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detailpenjualan`
+--
+
+CREATE TABLE `detailpenjualan` (
+  `DetailID` int NOT NULL,
+  `PenjualanID` int NOT NULL,
+  `ProdukID` int NOT NULL,
+  `JumlahProduk` int NOT NULL,
+  `Subtotal` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `detailpenjualan`
+--
+
+INSERT INTO `detailpenjualan` (`DetailID`, `PenjualanID`, `ProdukID`, `JumlahProduk`, `Subtotal`) VALUES
+(60, 25, 17, 2, 40000.00),
+(61, 25, 18, 1, 60000.00),
+(62, 26, 21, 2, 10000.00),
+(63, 27, 18, 2, 120000.00),
+(64, 28, 18, 1, 60000.00),
+(65, 29, 17, 4, 80000.00),
+(66, 29, 21, 10, 50000.00),
+(67, 30, 17, 2, 40000.00),
+(68, 30, 21, 2, 10000.00),
+(69, 31, 18, 1, 60000.00),
+(70, 32, 21, 2, 10000.00),
+(71, 33, 18, 100, 6000000.00),
+(72, 34, 21, 3, 15000.00),
+(73, 35, 22, 1, 35000.00);
+
+--
+-- Triggers `detailpenjualan`
+--
+DELIMITER $$
+CREATE TRIGGER `kurangiStok` AFTER INSERT ON `detailpenjualan` FOR EACH ROW UPDATE produk SET produk.Stok = produk.Stok - NEW.JumlahProduk
+WHERE produk.ProdukID = NEW.ProdukID
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `nambahTotalHarga` AFTER INSERT ON `detailpenjualan` FOR EACH ROW UPDATE penjualan SET penjualan.TotalHarga = penjualan.TotalHarga + NEW.Subtotal
+WHERE penjualan.PenjualanID= NEW.PenjualanID
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `PelangganID` int NOT NULL,
+  `NamaPelanggan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `Alamat` text COLLATE utf8mb4_general_ci NOT NULL,
+  `NomorTelepon` varchar(30) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`PelangganID`, `NamaPelanggan`, `Alamat`, `NomorTelepon`) VALUES
+(13, 'lisa', 'bekasi', '088888'),
+(16, '-', '-', '-'),
+(17, 'Pak sani', 'bsd', '08123456'),
+(18, 'Jay', 'BEKASI', '085894837316');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `PenjualanID` int NOT NULL,
+  `TanggalPenjualan` date NOT NULL,
+  `TotalHarga` decimal(10,2) NOT NULL,
+  `PelangganID` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`PenjualanID`, `TanggalPenjualan`, `TotalHarga`, `PelangganID`) VALUES
+(25, '2025-02-08', 100000.00, 12),
+(26, '2025-02-09', 10000.00, 13),
+(27, '2025-02-10', 120000.00, 13),
+(28, '2025-02-10', 60000.00, 13),
+(29, '2025-02-10', 130000.00, 13),
+(30, '2025-02-10', 50000.00, 13),
+(31, '2025-02-10', 60000.00, 13),
+(32, '2025-02-10', 10000.00, 16),
+(33, '2025-02-11', 6000000.00, 13),
+(34, '2025-02-13', 15000.00, 13),
+(35, '2025-02-18', 35000.00, 13);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `ProdukID` int NOT NULL,
+  `NamaProduk` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `Harga` int NOT NULL,
+  `Stok` int NOT NULL,
+  `HargaBeli` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`ProdukID`, `NamaProduk`, `Harga`, `Stok`, `HargaBeli`) VALUES
+(18, 'Tumbler', 60000, 0, 50000),
+(21, 'mie indomie', 5000, 1, 3000),
+(22, 'Rokok LA Ice', 35000, 3, 30000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `email` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
+  `nama` varchar(150) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password` char(32) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `level` enum('admin','petugas') COLLATE utf8mb4_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`email`, `nama`, `password`, `level`) VALUES
+('nabil@gmail.com', 'Muhamad Nabil', '202cb962ac59075b964b07152d234b70', 'admin'),
+('robin@gmail.com', 'ahmad robin', '202cb962ac59075b964b07152d234b70', 'petugas');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `detailpenjualan`
+--
+ALTER TABLE `detailpenjualan`
+  ADD PRIMARY KEY (`DetailID`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`PelangganID`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`PenjualanID`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`ProdukID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `detailpenjualan`
+--
+ALTER TABLE `detailpenjualan`
+  MODIFY `DetailID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+
+--
+-- AUTO_INCREMENT for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `PelangganID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `PenjualanID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `ProdukID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
